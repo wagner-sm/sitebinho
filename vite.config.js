2ef66e26e43e1a2be6f905fa585e7b43169685cc
Wagner Moreira <wsmmetal@outlook.com>
2025-06-15 11:00:52 -0300

    Atualiza site

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // importante para deploy no Netlify
+  base: './'  // caminho relativo para assets
 })
